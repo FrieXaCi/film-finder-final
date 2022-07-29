75127c9438c98b5dd3ca431f1ff90c3d7dcfb8f2
Frieda Haringsma <fharingsma@hotmail.com>
2022-07-29 14:35:52 +0200

    bewaren

---
+++ script/bewaren.js
@@ -0,0 +1,18 @@
+// stop slider
+/* slide.addEventListener("transitionend", () => {
+  slides = document.querySelectorAll("li");
+  if (slides[index].id === addFirstClone()) {
+    slide.style.transition = "none";
+    index = 1;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  }
+  if (slides[index].id === addLastClone()) {
+    slide.style.transition = "none";
+    index = slides.length - 2;
+    slide.style.transform = `translateX(${-slideWidth * index}px)`;
+  } 
+});*/
+/* addMovie.addEventListener("transitionend", () => {
+  console.log("einde");
+  slide.style.transition = "none";
+}); */
